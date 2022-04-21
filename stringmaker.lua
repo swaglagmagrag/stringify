4a0e49edfe33dfacbbaf19e6b2bb6ca6b531f3d6
swaglagmagrag <75270704+swaglagmagrag@users.noreply.github.com>
2022-04-21 14:08:01 -0500

    Update stringmaker.lua

--- stringmaker.lua
+++ stringmaker.lua
@@ -1,7 +1,7 @@
 --Word seperator (Keep in mind, each word has to have a space inbetween them to create a new string for the table!)
-local words = "word word word word word "
-local p = ""
-local t = {}
+words = "word word word word word "
+p = ""
+t = {}
 for i = 1, #words do
   local c = words:sub(i,i)
   if c == " " then
@@ -18,11 +18,12 @@ end
 
 --Character seperator (This will seperate any character in the chars area into seperate strings for the table!)
 
-local chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=[]\':;?/>.<,|}{+_)(*&^%$#@!`~"
-local p = ""
-local t = {}
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=[]\':;?/>.<,|}{+_)(*&^%$#@!`~"
+p = ""
+t = {}
 for i = 1, #chars do
   local c = chars:sub(i,i)
+  p = c
   p = '"' .. p .. '", '
   table.insert(t, p)
   p = ""
